e87ffb0cd471cd7dc67b674d9f05a670c2b8e22f
jbolmarcich <jbolmarcich@gmail.com>
2016-03-24 11:56:31 -0400

    Added files via upload

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php echo head(array('bodyid'=>'home', 'bodyclass' =>'two-col')); ?>
+
 <div id="primary">
-    <?php if ($homepageText = get_theme_option('Homepage Text')): ?>
+	<?php if ($homepageText = get_theme_option('Homepage Text')): ?>
     <p><?php echo $homepageText; ?></p>
     <?php endif; ?>
     <?php if (get_theme_option('Display Featured Item') == 1): ?>
@@ -44,4 +45,5 @@
     <?php fire_plugin_hook('public_home', array('view' => $this)); ?>
 
 </div><!-- end secondary -->
+
 <?php echo foot(); ?>
